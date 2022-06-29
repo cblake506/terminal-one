b917ee95921d914336e9e1f13924232eb1719d11
Javier <javier.sardinas@gmail.com>
2022-06-29 18:59:49 -0500

    Set login UI in a diferent page and use middleware to redirect to login page when not logged in

--- public/js/login.js
+++ public/js/login.js
@@ -14,8 +14,7 @@ const loginFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            //document.location.replace('/notes');
-            document.location.reload();
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
@@ -39,7 +38,7 @@ const signupFormHandler = async (event) => {
         });
   
         if (response.ok) {
-            document.location.reload();
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
