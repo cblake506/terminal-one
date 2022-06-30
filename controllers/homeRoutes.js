1bc3e89bdd09d9419c0be402630b6108d4604917
Javier <javier.sardinas@gmail.com>
2022-06-29 19:07:24 -0500

    Missing controller from previous commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
 const { Note, User } = require('../models');
-//const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
-    console.log(req.session.logged_in);
     res.render('homepage',{
       logged_in: req.session.logged_in,
       userName: req.session.userName
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
   } catch (err) {
     res.status(500).json(err);
   }
