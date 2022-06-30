a3d8f3907168deee7328e6fac6af237408aaa97b
Javier <javier.sardinas@gmail.com>
2022-06-29 21:07:09 -0500

    Add mock notes to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,9 +4,14 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
+    // const userData = await User.findByPk(req.session.user_id, {
+    //   // JOIN with users, using the Users_Notes through table
+    //   include: [{ model: Note, through: Users_Notes, as: 'note-to-user' }]
+    // });
     res.render('homepage',{
       logged_in: req.session.logged_in,
-      userName: req.session.userName
+      userName: req.session.userName,
+      notes: [{title:'note1', content:'content1'}, {title:'note2', content:'content2'}, {title:'note3', content:'content3'}]
     });
   } catch (err) {
     res.status(500).json(err);
